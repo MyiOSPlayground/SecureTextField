d8e2517fa06182144518d781623a0f5899872faa
Hanwe Lee <mobiledev@kakao.com>
2022-04-23 17:33:52 +0900

    작업중

---
+++ SecureTextField/SecureTextField/SecureTextField.swift
@@ -0,0 +1,132 @@
+//
+//  SecureTextField.swift
+//  SecureTextField
+//
+//  Created by hanwe on 2022/04/23.
+//
+
+import UIKit
+import SnapKit
+
+final class SecureTextField: UIView {
+    
+    // MARK: private property
+    
+    private let keyName: String = "SecureTextFieldSecureEnclaveKey"
+    
+    // MARK: private UI property
+    
+    private lazy var textField: UITextField = {
+        let textField: UITextField = UITextField()
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    private var key: SecKey?
+    private var tempSavedString: String? // TODO: 나중에 메모리에 남지 않는 스트링으로 바꾸자.
+    private var beforeTextFieldTextCount: Int = 0 // 필요가 있을까?
+    private var encryptedTextData: Data = Data() // TODO: 나중에 메모리에 남지 않는 스트링으로 바꾸자.
+    
+    // MARK: internal property
+    
+    // MARK: lifeCycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    // MARK: private function
+    
+    private func setup() {
+        initUI()
+        prepareKey()
+        textField.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
+    }
+    
+    private func initUI() {
+        
+        self.backgroundColor = .clear
+        
+        self.addSubview(self.textField)
+        self.textField.snp.makeConstraints {
+            $0.edges.equalTo(self)
+        }
+    }
+    
+    private func prepareKey() {
+        self.key = KeyChainHelper.loadKey(name: self.keyName)
+        if key == nil {
+            do {
+                key = try KeyChainHelper.makeAndStoreKey(name: keyName,
+                                                         requiresBiometry: false)
+            } catch let err {
+                print("SecureTextField Error. can't make key: \(err.localizedDescription)")
+            }
+        }
+    }
+    
+    private func encString(_ input: String) -> Data? {
+        guard let key = self.key else { return nil }
+        guard let publicKey = SecKeyCopyPublicKey(key) else { return nil }
+        let encAlgorithm: SecKeyAlgorithm = .eciesEncryptionCofactorVariableIVX963SHA256AESGCM
+        guard SecKeyIsAlgorithmSupported(publicKey, .encrypt, encAlgorithm) else { return nil }
+        var error: Unmanaged<CFError>?
+        guard let clearTextData = input.data(using: .utf8) else { return nil }
+        guard let cipherTextData = SecKeyCreateEncryptedData(publicKey, encAlgorithm, clearTextData as CFData, &error) as? Data else { return nil }
+        return cipherTextData
+    }
+    
+    private func decString(_ encData: Data) -> String? {
+        guard let key = key else { return nil }
+        let decAlgorithm: SecKeyAlgorithm = .eciesEncryptionCofactorVariableIVX963SHA256AESGCM
+        guard SecKeyIsAlgorithmSupported(key, .decrypt, decAlgorithm) else {
+            return nil
+        }
+        var error: Unmanaged<CFError>?
+        guard let decTextData = SecKeyCreateDecryptedData(key, decAlgorithm, encData as CFData, &error) as? Data else { return nil }
+        if error != nil { return nil }
+        guard let returnString: String = String(data: decTextData, encoding: .utf8) else { return nil }
+        return returnString
+    }
+    
+    @objc private func textFieldDidChange(_ textField: UITextField) {
+        guard let inputedText = textField.text else { return }
+        
+        if inputedText.count > self.beforeTextFieldTextCount { // 텍스트가 추가된 경우
+            let addedText: String = String(inputedText.last ?? Character.init(""))
+            let decStr = decString(self.encryptedTextData) ?? ""
+            print("복호화된것 : \(decStr)")
+            print("이제 원문임: \(decStr + addedText)")
+            guard let encData = encString(decStr + addedText) else { return }
+            print("다시 암호화 : \(encData)")
+            self.encryptedTextData = encData
+            self.textField.text?.removeLast()
+            self.textField.text = (self.textField.text ?? "") + "-"
+        } else if inputedText.count < self.beforeTextFieldTextCount { // 텍스트가 지워진 경우
+            
+        } else { // 텍스트가 같다..? 안들어올것 같은 경우
+            
+        }
+        
+        
+        
+        
+        
+        self.encryptedTextData = encString(textField.text!)!
+        print("enc ed: \(self.encryptedTextData)")
+        
+        self.beforeTextFieldTextCount = textField.text?.count ?? 0
+    }
+
+    
+    // MARK: internal function
+    
+    // MARK: action
+
+}
